d521e172bec1bc54a4b94135e95e2c7e6e17581b
Viktoria Petrenko <vikuacz@seznam.cz>
2019-10-08 20:36:26 +0200

    004-interacting-with-the-Yelp-API: added searchYelp method with fetch

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,1 +1,19 @@
-const test = process.env.REACT_APP_YELP_API_KEY;
+const apiKey = process.env.REACT_APP_YELP_API_KEY;
+const Yelp = {
+searchYelp (term,location,sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{
+        headers:{
+            Authorization:`Bearer ${apiKey}`
+        }
+    }).then(response => {
+        return response.json();
+    }).then(jsonResponse=>{
+if (jsonResponse.businesses ){
+}else {
+    return jsonResponse.businesses.map(business=>{
+
+    });
+}
+    });
+}
+};
