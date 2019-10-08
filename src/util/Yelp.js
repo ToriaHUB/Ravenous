b1a33cfcf191e57b2dfc276d7c5f101ec98803a2
Viktoria Petrenko <vikuacz@seznam.cz>
2019-10-08 22:05:42 +0200

    004-interacting-with-the-Yelp-API: accessing the JSON Response

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,19 +1,37 @@
 const apiKey = process.env.REACT_APP_YELP_API_KEY;
-const Yelp = {
-searchYelp (term,location,sortBy) {
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,{
-        headers:{
-            Authorization:`Bearer ${apiKey}`
+export const yelp = {
+  searchYelp(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`
         }
-    }).then(response => {
+      }
+    )
+      .then(response => {
         return response.json();
-    }).then(jsonResponse=>{
-if (jsonResponse.businesses ){
-}else {
-    return jsonResponse.businesses.map(business=>{
+      })
+      .then(jsonResponse => {
+        if (jsonResponse.businesses) {
+        } else {
+          return jsonResponse.businesses.map(business => {
+            console.log(business);
 
-    });
-}
-    });
-}
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.address.location,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zipCode,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count
+            };
+          });
+        }
+      });
+  }
 };
