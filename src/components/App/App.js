86a9533131982db6ebfc63bd517d5d53c33223e8
Viktoria Petrenko <vikuacz@seznam.cz>
2019-10-11 17:03:48 +0200

    004-interacting-with-the-Yelp-API: changed the default language settings fog Google map

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -37,7 +37,7 @@ export class App extends React.Component {
         <BusinessList businesses={this.state.businesses} />
         </Route>
             <Route path="/target-map" exact={true} render={props=> <TargetPlaceMap {...props}
-                                                                                   googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAP_GOOGLE_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
+                                                                                   googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAP_GOOGLE_API_KEY}&language=en&v=3.exp&libraries=geometry,drawing,places`}
                                                                                    loadingElement={<div style={{ height: `100%` }} />}
                                                                                    containerElement={<div style={{ height: `400px` }} />}
                                                                                    mapElement={<div style={{ height: `100%` }} />}/>}/>
--- src/components/Map/TargetPlaceMap.js
+++ src/components/Map/TargetPlaceMap.js
@@ -28,36 +28,3 @@ export const TargetPlaceMap = withScriptjs(withGoogleMap(props => {
 
 
 
-
-
-
-
-
-
-
-// const Map=(props)=>{
-//     console.log(props);
-//     const coordinates={
-//         lat:props.history.location.state.lat,
-//         lng:props.history.location.state.lng,
-//         };
-//
-//     return(
-//         <GoogleMap defaultZoom={10}
-//                    defaultCenter={{lat:coordinates.lat,lng:coordinates.lng,}}/>
-//
-//     )
-// };
-// const WrappedMap = withScriptjs(withGoogleMap(Map));
-//
-// export const TargetPlaceMap=(props)=> {
-//             return(
-//             <div style={{width:'100vw', height:'100vh'}}>
-//                 <WrappedMap googleMapUrl={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
-//                             loadingElement={<div style={{ height: `100%` }} />}
-//                             containerElement={<div style={{ height: `400px` }} />}
-//                             mapElement={<div style={{ height: `100%` }} />}
-//                             props={props}/>
-//             </div>
-//         )
-// };
